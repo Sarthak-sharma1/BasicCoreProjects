e529f45308873077e25ca14bfb27c488986d61c3
codewithsarthak102 <sarthakpandat781@gmail.com>
2022-12-01 19:29:50 +0530

    Basic Java programs.

---
+++ src/FirstPrograms/PrimeNumber.java
@@ -0,0 +1,21 @@
+package FirstPrograms;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int n,count=0;
+        System.out.println("Enter any number");
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+      for (int i=1;i<=n;i++){
+          if (n%i==0){
+              count++;
+          }
+      }
+      if (count==2){
+          System.out.println("Prime Number");
+      }else System.out.println("Error");
+    }
+
+}
