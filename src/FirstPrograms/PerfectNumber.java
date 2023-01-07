3db12c28e6a331398a96185a7989d9e2c5b118e8
codewithsarthak102 <sarthakpandat781@gmail.com>
2023-01-07 11:26:06 +0530

    Basic Java programs.

---
+++ src/FirstPrograms/PerfectNumber.java
@@ -0,0 +1,22 @@
+package FirstPrograms;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        int n,sum=0;
+        System.out.println("Enter any Number");
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        for (int i = 1; i <n; i++) {
+            if (n%i==0){
+                sum = sum + i;
+            }
+
+        }
+        if (n==sum){
+            System.out.println("Perfect Number");
+        }else
+            System.out.println("Error");
+    }
+}
